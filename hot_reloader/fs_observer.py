0cc3636faf34f5347e4fe56f53e97b34584cb247
Anton Bezrukov <hello@antonbezrukov.ru>
2025-01-14 23:12:16 +0300

    Handle exceptions

--- hot_reloader/fs_observer.py
+++ hot_reloader/fs_observer.py
@@ -23,16 +23,20 @@ class _FileChangeEventHandler(FileSystemEventHandler):
         src_path = event.src_path
         log.debug(f"New file system event: {event}")
 
-        if re.match(r"^[^#]+\.py$", src_path):
-            file_path = pathlib.Path(src_path)
-            rel_path = file_path.relative_to(self._root_parent_path).as_posix()
-            mod_name = path_to_module(rel_path)
-            log.debug(f"Python file has been changed. file_path={file_path}, root_relative_path={rel_path}")
-            log.debug(f"Reload modules after {mod_name} change")
-            hot_reload_modules_dependend_on(mod_name, self._root_dir)
-            if self._after_reload_hook:
-                log.debug(f"Calling after reload hook for {mod_name} change")
-                self._after_reload_hook(mod_name)
+        try:
+            if re.match(r"^[^#]+\.py$", src_path):
+                file_path = pathlib.Path(src_path)
+                rel_path = file_path.relative_to(self._root_parent_path).as_posix()
+                mod_name = path_to_module(rel_path)
+                log.debug(f"Python file has been changed. file_path={file_path}, root_relative_path={rel_path}")
+                log.debug(f"Reload modules after {mod_name} change")
+                hot_reload_modules_dependend_on(mod_name, self._root_dir)
+                if self._after_reload_hook:
+                    log.debug(f"Calling after reload hook for {mod_name} change")
+                    self._after_reload_hook(mod_name)
+        except Exception as e:
+            log.exception("hot reload exception!")
+            log.exception(e)
 
 
 def start_hot_reloading(root_dir="hot_reload", after_reload_hook=None):
